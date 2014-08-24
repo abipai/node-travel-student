292be7d518e488d9889ac33f1914a730144e0d0f
Abi Pai <abi@nodesoure.com>
2014-08-24 18:56:33 +0000

    changed my first commit

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "YOUR_NAME_HERE";
+	var myName = "Abi";
 	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
+		resp.write("<html><body><h1>Welcome to "+myName+"'s first, really sweet, web app!</body></html>");
 		resp.end();
 	});
 
